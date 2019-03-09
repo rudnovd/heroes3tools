9682ff542144aa2e6da9fde3cf63b4ad7371ea1a
Hyuugo <rudnov.d@gmail.com>
2019-03-09 22:04:28 +0500

    Calculation with some heroes spells specialty, started divide components

--- src/components/DamageCalculator.vue
+++ src/components/DamageCalculator.vue
@@ -44,12 +44,7 @@
             //- Choose ATTACKER hero
             b-row.mt-3()
               b-col(cols='12')
-                v-select(
-                  v-model='data.attacker.hero.hero'
-                  :options='data.heroes'
-                  label='name'
-                  placeholder='Hero'
-                )
+                select-hero(@sendHero='selectHero("attacker", $event)')
 
         //- Inputs ATTACKER level and stats
         b-row.mt-2()
@@ -294,12 +289,7 @@
             //- Choose DEFENDER hero
             b-row.mt-3()
               b-col(cols='12')
-                v-select(
-                  v-model='data.defender.hero.hero'
-                  :options='data.heroes'
-                  label='name'
-                  placeholder='Hero'
-                )
+                select-hero(@sendHero='selectHero("defender", $event)')
 
           //- Pick DEFENDER unit image
           b-col(cols='3' sm='3' md='3' lg='3' xl='3')
@@ -650,10 +640,13 @@
 <script>
 import unitsJson from '@/assets/json/units.json'
 
-import heroesJson from '@/assets/json/heroes.json'
+import selectHero from '@/components/damageCalculator/selectHero.vue'
 
 export default {
   name: 'DamageCalculator',
+  components: {
+    'select-hero': selectHero
+  },
   beforeCreate () {
     document.title = 'Damage calculator'
   },
@@ -678,8 +671,6 @@ export default {
             neutral: []
           }
         },
-
-        heroes: heroesJson,
 
         skillsOptions: [
           { text: 'None', value: 0 },
@@ -787,6 +778,12 @@ export default {
     }
   },
   methods: {
+    selectHero (side, hero) {
+      if (side === 'attacker') this.data.attacker.hero.hero = hero
+      else if (side === 'defender') this.data.defender.hero.hero = hero
+
+      this.calculate()
+    },
     // Select unit
     selectAttacker (unit) {
       this.data.attacker.unit = unit
@@ -1001,6 +998,9 @@ export default {
       defenderAttack += parseInt(this.data.defender.hero.attack)
       defenderDefense += parseInt(this.data.defender.hero.defense)
 
+      const attackerHero = this.data.attacker.hero.hero
+      const defenderHero = this.data.defender.hero.hero
+
       const attackerAir = this.data.attacker.hero.skills.airLevel
       const attackerFire = this.data.attacker.hero.skills.fireLevel
       const attackerEarth = this.data.attacker.hero.skills.earthLevel
@@ -1018,6 +1018,13 @@ export default {
 
         // Bloodlust spell
         if (effects.includes('Bloodlust')) {
+          // Bloodlust spell hero specialty bonus
+          if (attackerHero.specialtySpell === 'bloodlust') {
+            if (this.data.attacker.unit.level <= 2) attackerAttack += 3
+            else if (this.data.attacker.unit.level <= 4) attackerAttack += 2
+            else if (this.data.attacker.unit.level <= 6) attackerAttack += 1
+          }
+
           if (attackerFire < 2) attackerAttack += 3
           else if (attackerFire > 1) attackerAttack += 6
         }
@@ -1042,6 +1049,20 @@ export default {
 
         // Prayer spell
         if (effects.includes('Prayer')) {
+          // Prayer spell hero specialty bonus
+          if (attackerHero.specialtySpell === 'prayer') {
+            if (this.data.attacker.unit.level <= 2) {
+              attackerAttack += 3
+              attackerDefense += 3
+            } else if (this.data.attacker.unit.level <= 4) {
+              attackerAttack += 2
+              attackerDefense += 2
+            } else if (this.data.attacker.unit.level <= 6) {
+              attackerAttack += 1
+              attackerDefense += 1
+            }
+          }
+
           if (attackerWater < 2) {
             attackerAttack += 2
             attackerDefense += 2
@@ -1053,12 +1074,26 @@ export default {
 
         // Precision spell
         if (effects.includes('Precision') && this.data.attacker.unit.ranged) {
+          // Precision hero specialty bonus
+          if (attackerHero.specialtySpell === 'precision') {
+            if (this.data.attacker.unit.level <= 2) attackerAttack += 3
+            else if (this.data.attacker.unit.level <= 4) attackerAttack += 2
+            else if (this.data.attacker.unit.level <= 6) attackerAttack += 1
+          }
+
           if (attackerAir < 2) attackerAttack += 3
           else if (attackerAir > 1) attackerAttack += 6
         }
 
         // Stone skin spell
         if (effects.includes('Stone Skin')) {
+          // Stone skin hero specialty bonus
+          if (attackerHero.specialtySpell === 'stone skin') {
+            if (this.data.attacker.unit.level <= 2) attackerDefense += 3
+            else if (this.data.attacker.unit.level <= 4) attackerDefense += 2
+            else if (this.data.attacker.unit.level <= 6) attackerDefense += 1
+          }
+
           if (attackerEarth < 2) attackerDefense += 3
           else if (attackerEarth > 1) attackerDefense += 6
         }
@@ -1067,12 +1102,22 @@ export default {
 
         // Weakness spell
         if (effects.includes('Weakness')) {
+          // Weakness hero specialty bonus
+          if (defenderHero.specialtySpell === 'weakness') {
+            if (this.data.attacker.unit.level <= 2) attackerAttack -= 3
+            else if (this.data.attacker.unit.level <= 4) attackerAttack -= 2
+            else if (this.data.attacker.unit.level <= 6) attackerAttack -= 1
+          }
+
           if (defenderWater < 2) attackerAttack -= 3
           else if (defenderWater > 1) attackerAttack -= 6
         }
 
         // Disrupting Ray spell
         if (effects.includes('Disrupting Ray')) {
+          // Disrupting Ray hero specialty bonus
+          if (defenderHero.specialtySpell === 'disrupting ray') attackerDefense -= 2
+
           if (defenderAir < 2) attackerDefense -= 3
           else if (defenderAir === 2) attackerDefense -= 4
           else if (defenderAir === 3) attackerDefense -= 5
@@ -1086,6 +1131,12 @@ export default {
 
         // Bloodlust spell
         if (effects.includes('Bloodlust')) {
+          if (defenderHero.specialtySpell === 'bloodlust') {
+            if (this.data.defender.unit.level <= 2) defenderAttack += 3
+            else if (this.data.defender.unit.level <= 4) defenderAttack += 2
+            else if (this.data.defender.unit.level <= 6) defenderAttack += 1
+          }
+
           if (defenderFire < 2) defenderAttack += 3
           else if (defenderFire > 1) defenderAttack += 6
         }
@@ -1110,6 +1161,20 @@ export default {
 
         // Prayer spell
         if (effects.includes('Prayer')) {
+          // Prayer spell hero specialty bonus
+          if (defenderHero.specialtySpell === 'prayer') {
+            if (this.data.defender.unit.level <= 2) {
+              defenderAttack += 3
+              defenderDefense += 3
+            } else if (this.data.defender.unit.level <= 4) {
+              defenderAttack += 2
+              defenderDefense += 2
+            } else if (this.data.defender.unit.level <= 6) {
+              defenderAttack += 1
+              defenderDefense += 1
+            }
+          }
+
           if (defenderWater < 2) {
             defenderAttack += 2
             defenderDefense += 2
@@ -1121,12 +1186,25 @@ export default {
 
         // Precision spell
         if (effects.includes('Precision') && this.data.defender.unit.ranged) {
+          if (defenderHero.specialtySpell === 'precision') {
+            if (this.data.defender.unit.level <= 2) defenderAttack += 3
+            else if (this.data.defender.unit.level <= 4) defenderAttack += 2
+            else if (this.data.defender.unit.level <= 6) defenderAttack += 1
+          }
+
           if (defenderAir < 2) defenderAttack += 3
           else if (defenderAir > 1) defenderAttack += 6
         }
 
         // Stone skin spell
         if (effects.includes('Stone Skin')) {
+          // Stone skin hero specialty bonus
+          if (defenderHero.specialtySpell === 'stone skin') {
+            if (this.data.defender.unit.level <= 2) defenderDefense += 3
+            else if (this.data.defender.unit.level <= 4) defenderDefense += 2
+            else if (this.data.defender.unit.level <= 6) defenderDefense += 1
+          }
+
           if (defenderEarth < 2) defenderDefense += 3
           else if (defenderEarth > 1) defenderDefense += 6
         }
@@ -1135,12 +1213,22 @@ export default {
 
         // Weakness spell
         if (effects.includes('Weakness')) {
+          // Weakness hero specialty bonus
+          if (attackerHero.specialtySpell === 'weakness') {
+            if (this.data.defender.unit.level <= 2) defenderAttack -= 3
+            else if (this.data.defender.unit.level <= 4) defenderAttack -= 2
+            else if (this.data.defender.unit.level <= 6) defenderAttack -= 1
+          }
+
           if (attackerWater < 2) defenderAttack -= 3
           else if (attackerWater > 1) defenderAttack -= 6
         }
 
         // Disrupting Ray spell
         if (effects.includes('Disrupting Ray')) {
+          // Disrupting Ray hero specialty bonus
+          if (attackerHero.specialtySpell === 'disrupting ray') defenderDefense -= 2
+
           if (attackerAir < 2) defenderDefense -= 3
           else if (attackerAir === 2) defenderDefense -= 4
           else if (attackerAir === 3) defenderDefense -= 5
@@ -1158,6 +1246,7 @@ export default {
       return { attackerAttack, attackerDefense, defenderAttack, defenderDefense }
     },
     calculateDamagesModification (attackerMinDamage, attackerMaxDamage, defenderMinDamage, defenderMaxDamage) {
+      const attackerHero = this.data.attacker.hero.hero
       // const attackerAir = this.data.attacker.hero.skills.airLevel
       const attackerFire = this.data.attacker.hero.skills.fireLevel
       // const attackerEarth = this.data.attacker.hero.skills.earthLevel
@@ -1174,8 +1263,11 @@ export default {
 
         // Bless spell
         if (effects.includes('Bless')) {
-          if (attackerWater < 2) attackerMinDamage = attackerMaxDamage
-          else if (attackerWater > 1) attackerMinDamage = attackerMaxDamage + 1
+          if (attackerHero.specialtySpell === 'bless') {
+          } else {
+            if (attackerWater < 2) attackerMinDamage = attackerMaxDamage
+            else if (attackerWater > 1) attackerMinDamage = attackerMaxDamage + 1
+          }
         }
 
         // Curse spell
@@ -1212,27 +1304,75 @@ export default {
     calculateTotalDamagesModification (attackerTotalMinDamage, attackerTotalMaxDamage, defenderTotalMinDamage, defenderTotalMaxDamage) {
       // For ATTACKER
 
+      const attackerHero = this.data.attacker.hero.hero
+      const defenderHero = this.data.defender.hero.hero
+
+      let attackerSkillBonus = 0
+      let defenderSkillBonus = 0
+
+      if (attackerHero) {
+        attackerSkillBonus = 1 + 0.05 * attackerHero.level
+      }
+      if (defenderHero) {
+        defenderSkillBonus = 1 + 0.05 * defenderHero.level
+      }
+
       // Offense ATTACKER skill bonus
       if (this.data.attacker.hero.skills.offenseLevel > 0 && !this.data.attacker.unit.ranged) {
-        attackerTotalMinDamage += (attackerTotalMinDamage * 0.1) * this.data.attacker.hero.skills.offenseLevel
-        attackerTotalMaxDamage += (attackerTotalMaxDamage * 0.1) * this.data.attacker.hero.skills.offenseLevel
+        let bonus = 0.1 * this.data.attacker.hero.skills.offenseLevel
+        if (attackerHero.specialtySkill === 'offense') {
+          bonus += attackerSkillBonus
+          if (bonus <= 1.92) {
+            attackerTotalMinDamage += attackerTotalMinDamage * bonus
+            attackerTotalMaxDamage += attackerTotalMaxDamage * bonus
+          } else if (bonus > 1.92) {
+            attackerTotalMinDamage += attackerTotalMinDamage * 1.92
+            attackerTotalMaxDamage += attackerTotalMaxDamage * 1.92
+          }
+        } else {
+          attackerTotalMinDamage += attackerTotalMinDamage * bonus
+          attackerTotalMaxDamage += attackerTotalMaxDamage * bonus
+        }
       }
 
       // Armorer ATTACKER skill bonus
       if (this.data.attacker.hero.skills.armorerLevel > 0) {
-        defenderTotalMinDamage -= (defenderTotalMinDamage * 0.05) * this.data.attacker.hero.skills.armorerLevel
-        defenderTotalMaxDamage -= (defenderTotalMaxDamage * 0.05) * this.data.attacker.hero.skills.armorerLevel
+        let bonus = 0.05 * this.data.attacker.hero.skills.armorerLevel
+        if (attackerHero.specialtySkill === 'armorer') {
+          bonus += attackerSkillBonus
+          if (bonus <= 0.7) {
+            defenderTotalMinDamage -= defenderTotalMinDamage * bonus
+            defenderTotalMaxDamage -= defenderTotalMaxDamage * bonus
+          } else if (bonus > 0.7) {
+            defenderTotalMinDamage -= defenderTotalMinDamage * 0.7
+            defenderTotalMaxDamage -= defenderTotalMaxDamage * 0.7
+          }
+        } else {
+          defenderTotalMinDamage -= defenderTotalMinDamage * bonus
+          defenderTotalMaxDamage -= defenderTotalMaxDamage * bonus
+        }
       }
 
       // Archery ATTACKER skill bonus
       if (this.data.attacker.hero.skills.archeryLevel > 0 && this.data.attacker.unit.ranged) {
         let bonus = 0
         if (this.data.attacker.hero.skills.archeryLevel === 1) bonus = 0.1
-        if (this.data.attacker.hero.skills.archeryLevel === 2) bonus = 0.25
-        if (this.data.attacker.hero.skills.archeryLevel === 3) bonus = 0.5
+        else if (this.data.attacker.hero.skills.archeryLevel === 2) bonus = 0.25
+        else if (this.data.attacker.hero.skills.archeryLevel === 3) bonus = 0.5
 
-        attackerTotalMinDamage += attackerTotalMinDamage * bonus
-        attackerTotalMaxDamage += attackerTotalMaxDamage * bonus
+        if (attackerHero.specialtySkill === 'archery') {
+          bonus += attackerSkillBonus
+          if (bonus <= 3.2) {
+            attackerTotalMinDamage += attackerTotalMinDamage * bonus
+            attackerTotalMaxDamage += attackerTotalMaxDamage * bonus
+          } else if (bonus > 3.2) {
+            attackerTotalMinDamage += attackerTotalMinDamage * 3.2
+            attackerTotalMaxDamage += attackerTotalMaxDamage * 3.2
+          }
+        } else {
+          attackerTotalMinDamage += attackerTotalMinDamage * bonus
+          attackerTotalMaxDamage += attackerTotalMaxDamage * bonus
+        }
       }
 
       // Artillery ATTACKER skill bonus (for ballista and cannon only)
@@ -1258,25 +1398,60 @@ export default {
 
       // Offense DEFENDER skill bonus
       if (this.data.defender.hero.skills.offenseLevel > 0 && !this.data.defender.unit.ranged) {
-        defenderTotalMinDamage += (attackerTotalMinDamage * 0.1) * this.data.defender.hero.skills.offenseLevel
-        defenderTotalMaxDamage += (attackerTotalMaxDamage * 0.1) * this.data.defender.hero.skills.offenseLevel
+        let bonus = 0.1 * this.data.defender.hero.skills.offenseLevel
+        if (defenderHero.specialtySkill === 'offense') {
+          bonus += defenderSkillBonus
+          if (bonus <= 1.92) {
+            defenderTotalMinDamage += defenderTotalMinDamage * bonus
+            defenderTotalMaxDamage += defenderTotalMaxDamage * bonus
+          } else if (bonus > 1.92) {
+            defenderTotalMinDamage += defenderTotalMinDamage * 1.92
+            defenderTotalMaxDamage += defenderTotalMaxDamage * 1.92
+          }
+        } else {
+          defenderTotalMinDamage += defenderTotalMinDamage * bonus
+          defenderTotalMaxDamage += defenderTotalMaxDamage * bonus
+        }
       }
 
       // Armorer DEFENDER skill bonus
       if (this.data.defender.hero.skills.armorerLevel > 0) {
-        attackerTotalMinDamage -= (defenderTotalMinDamage * 0.05) * this.data.defender.hero.skills.armorerLevel
-        attackerTotalMaxDamage -= (defenderTotalMaxDamage * 0.05) * this.data.defender.hero.skills.armorerLevel
+        let bonus = 0.05 * this.data.defender.hero.skills.armorerLevel
+        if (defenderHero.specialtySkill === 'armorer') {
+          bonus += defenderSkillBonus
+          if (bonus <= 0.7) {
+            attackerTotalMinDamage -= attackerTotalMinDamage * bonus
+            attackerTotalMaxDamage -= attackerTotalMaxDamage * bonus
+          } else if (bonus > 0.7) {
+            attackerTotalMinDamage -= attackerTotalMinDamage * 0.7
+            attackerTotalMaxDamage -= attackerTotalMaxDamage * 0.7
+          }
+        } else {
+          attackerTotalMinDamage -= attackerTotalMinDamage * bonus
+          attackerTotalMaxDamage -= attackerTotalMaxDamage * bonus
+        }
       }
 
       // Archery DEFENDER skill bonus
       if (this.data.defender.hero.skills.archeryLevel > 0 && this.data.attacker.unit.ranged) {
         let bonus = 0
         if (this.data.defender.hero.skills.archeryLevel === 1) bonus = 0.1
-        if (this.data.defender.hero.skills.archeryLevel === 2) bonus = 0.25
-        if (this.data.defender.hero.skills.archeryLevel === 3) bonus = 0.5
+        else if (this.data.defender.hero.skills.archeryLevel === 2) bonus = 0.25
+        else if (this.data.defender.hero.skills.archeryLevel === 3) bonus = 0.5
 
-        defenderTotalMinDamage += attackerTotalMinDamage * bonus
-        defenderTotalMaxDamage += attackerTotalMaxDamage * bonus
+        if (defenderHero.specialtySkill === 'archery') {
+          bonus += defenderSkillBonus
+          if (bonus <= 3.2) {
+            defenderTotalMinDamage += defenderTotalMinDamage * bonus
+            defenderTotalMaxDamage += defenderTotalMaxDamage * bonus
+          } else if (bonus > 3.2) {
+            defenderTotalMinDamage += defenderTotalMinDamage * 3.2
+            defenderTotalMaxDamage += defenderTotalMaxDamage * 3.2
+          }
+        } else {
+          defenderTotalMinDamage += defenderTotalMinDamage * bonus
+          defenderTotalMaxDamage += defenderTotalMaxDamage * bonus
+        }
       }
 
       // Artillery DEFENDER skill bonus (for ballista and cannon only)
---
+++ src/components/damageCalculator/selectHero.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+v-select(
+  v-model='data.hero'
+  :options='data.heroes'
+  label='name'
+  placeholder='Hero'
+  @input='sendHero(data.hero)'
+)
+</template>
+
+<script>
+import heroesJson from '@/assets/json/heroes.json'
+
+export default {
+  name: 'selectHero',
+  data () {
+    return {
+      data: {
+        heroes: heroesJson,
+        hero: null
+      }
+    }
+  },
+  methods: {
+    sendHero (hero) {
+      this.$emit('sendHero', hero)
+    }
+  }
+}
+
+</script>
