518c1bc7248b2d23140aac355ea60fc60933f00c
Hyuugo <rudnov.d@gmail.com>
2019-03-14 13:31:02 +0500

    update store for calculator

--- src/store/modules/calculator/actions.js
+++ src/store/modules/calculator/actions.js
@@ -4,5 +4,44 @@ export default {
   },
   getHeroes ({ commit }) {
     commit('GET_HEROES')
+  },
+
+  setHero ({ commit }, { side, hero }) {
+    if (side === 'attacker') {
+      commit('CLEAR_ATTACKER_HERO')
+      commit('SET_ATTACKER_HERO', hero)
+    } else if (side === 'defender') {
+      commit('CLEAR_DEFENDER_HERO')
+      commit('SET_DEFENDER_HERO', hero)
+    }
+  },
+
+  setHeroLevel ({ commit }, { side, level }) {
+    if (side === 'attacker') commit('SET_ATTACKER_HERO_LEVEL', level)
+    else if (side === 'defender') commit('SET_DEFENDER_HERO_LEVEL', level)
+  },
+  setHeroAttack ({ commit }, { side, attack }) {
+    if (side === 'attacker') commit('SET_ATTACKER_HERO_ATTACK', attack)
+    else if (side === 'defender') commit('SET_DEFENDER_HERO_ATTACK', attack)
+  },
+  setHeroDefense ({ commit }, { side, defense }) {
+    if (side === 'attacker') commit('SET_ATTACKER_HERO_DEFENSE', defense)
+    else if (side === 'defender') commit('SET_DEFENDER_HERO_DEFENSE', defense)
+  },
+
+  setHeroSkill ({ commit }, { side, skill, level }) {
+    if (side === 'attacker') commit('SET_ATTACKER_HERO_SKILL', { skill, level })
+    else if (side === 'defender') commit('SET_DEFENDER_HERO_SKILL', { skill, level })
+  },
+
+  setAttackerUnit ({ commit }, { unit }) {
+    commit('SET_ATTACKER_UNIT', unit)
+  },
+  setDefenderUnit ({ commit }, { unit }) {
+    commit('SET_DEFENDER_UNIT', unit)
+  },
+
+  swapHeroes ({ commit }) {
+    commit('SWAP_HEROES')
   }
 }
--- src/store/modules/calculator/getters.js
+++ src/store/modules/calculator/getters.js
@@ -4,5 +4,19 @@ export default {
   },
   getHeroes: state => {
     return state.heroes
+  },
+
+  getAttacker: state => {
+    return state.attacker
+  },
+  getDefender: state => {
+    return state.defender
+  },
+
+  getAttackerHero: state => {
+    return state.attacker.hero
+  },
+  getDefenderHero: state => {
+    return state.defender.hero
   }
 }
