9ca1021127e8b99b83a9e6aa2088656118625e19
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2019-03-16 02:41:00 +0500

    try to rewrite calculator for vuex

--- src/components/damageCalculator/pickUnitModal.vue
+++ src/components/damageCalculator/pickUnitModal.vue
@@ -14,21 +14,21 @@ b-modal(
 
     //- Search unit
     b-col.ml-2.mr-auto.mb-3(cols='5' sm='5' md='5' lg='4' xl='3')
-      b-form-input(v-model='data.search.text' type='text' placeholder='Angel' @input='searchUnit()')
+      b-form-input(v-model='search.text' type='text' placeholder='Angel' @input='searchUnit()')
     //- Clear unit
     b-col.mr-2.mr-sm-4.mr-md-4.mr-lg-5.mr-xl-5(cols='3' sm='2' md='2' lg='1' xl='1')
       b-btn(variant='danger' @click='hideModal(); clearSearch();')
         | Clear
 
     //- Show if not search
-    b-col(v-if='!data.search.text' cols='12' v-for='(castle, index) in Object.keys(data.units.list)' :key='castle.id')
-      b-btn(variant='link' size='sm' v-for='(unit, number) in data.units.list[castle]' :key='unit.id')
-        img(:src='unitsImages[unit.id].src' @click='selectUnit(unit)')
+    b-col(v-if='!search.text' cols='12' v-for='(castle, index) in Object.keys(units)' :key='castle.id')
+      b-btn(variant='link' size='sm' v-for='(unit, number) in units[castle]' :key='unit.id')
+        img(:src='unitsImages[unit.id].src' @click='setUnit({ side, unit }); hideModal();')
 
     //- Show if search
     b-col(v-else)
-      b-btn(variant='link' size='sm' v-for='(unit, index) in data.search.foundUnits' :key='unit.id')
-        img(:src='unitsImages[unit.id].src' @click='selectUnit(unit)')
+      b-btn(variant='link' size='sm' v-for='(unit, index) in search.foundUnits' :key='unit.id')
+        img(:src='unitsImages[unit.id].src' @click='setUnit({ side, unit}); hideModal();')
 
 </template>
 
@@ -59,124 +59,114 @@ export default {
   },
   data () {
     return {
-      data: {
-        units: {
-          list: {
-            castle: [],
-            rampart: [],
-            tower: [],
-            inferno: [],
-            necropolis: [],
-            dungeon: [],
-            stronghold: [],
-            fortress: [],
-            conflux: [],
-            cove: [],
-            neutral: []
-          }
-        },
+      units: {
+        castle: [],
+        rampart: [],
+        tower: [],
+        inferno: [],
+        necropolis: [],
+        dungeon: [],
+        stronghold: [],
+        fortress: [],
+        conflux: [],
+        cove: [],
+        neutral: []
+      },
 
-        images: [],
+      images: [],
 
-        search: {
-          text: null,
-          foundUnits: []
-        }
+      search: {
+        text: null,
+        foundUnits: []
       }
     }
   },
   methods: {
     ...mapActions({
-      setAttackerUnit: 'calculator/setAttackerUnit',
-      setDefenderUnit: 'calculator/setDefenderUnit'
+      setUnit: 'calculator/setUnit'
     }),
-    selectUnit (unit) {
-      if (this.side === 'attacker') this.setAttackerUnit(unit)
-      else if (this.side === 'defender') this.setDefenderUnit(unit)
-      this.hideModal(this.refString)
 
-      this.clearSearch()
+    showModal () {
+      let refs = this.refString
+      this.$refs[refs].show()
     },
-
-    showModal (refString) {
-      this.$refs[refString].show()
-    },
-    hideModal (refString) {
-      this.$refs[refString].hide()
+    hideModal () {
+      let refs = this.refString
+      this.$refs[refs].hide()
     },
 
     // Search units
     searchUnit () {
-      this.data.search.foundUnits = []
+      this.search.foundUnits = []
       for (let i = 0; i < unitsJson.length; i++) {
-        if (unitsJson[i].name.indexOf(this.data.search.text) !== -1 || unitsJson[i].name.toLowerCase().indexOf(this.data.search.text) !== -1) {
-          this.data.search.foundUnits.push(unitsJson[i])
+        if (!unitsJson[i].name.includes(this.search.text) || !unitsJson[i].name.toLowerCase().includes(this.search.text)) {
+          this.search.foundUnits.push(unitsJson[i])
         }
       }
     },
     clearSearch () {
-      this.data.search.text = null
-      this.data.search.foundUnits = []
+      this.search.text = null
+      this.search.foundUnits = []
     },
     jsonToData () {
       // Castle
       for (let i = 0; i < 14; i++) {
-        this.data.units.list.castle.push(unitsJson[i])
+        this.units.castle.push(unitsJson[i])
       }
 
       // Rampart
       for (let i = 14; i < 28; i++) {
-        this.data.units.list.rampart.push(unitsJson[i])
+        this.units.rampart.push(unitsJson[i])
       }
 
       // Tower
       for (let i = 28; i < 42; i++) {
-        this.data.units.list.tower.push(unitsJson[i])
+        this.units.tower.push(unitsJson[i])
       }
 
       // Necropolis
       for (let i = 42; i < 56; i++) {
-        this.data.units.list.necropolis.push(unitsJson[i])
+        this.units.necropolis.push(unitsJson[i])
       }
 
       // Dungeon
       for (let i = 56; i < 70; i++) {
-        this.data.units.list.dungeon.push(unitsJson[i])
+        this.units.dungeon.push(unitsJson[i])
       }
 
       // Inferno
       for (let i = 70; i < 84; i++) {
-        this.data.units.list.inferno.push(unitsJson[i])
+        this.units.inferno.push(unitsJson[i])
       }
 
       // Stronghold
       for (let i = 84; i < 98; i++) {
-        this.data.units.list.stronghold.push(unitsJson[i])
+        this.units.stronghold.push(unitsJson[i])
       }
 
       // Fortress
       for (let i = 98; i < 112; i++) {
-        this.data.units.list.fortress.push(unitsJson[i])
+        this.units.fortress.push(unitsJson[i])
       }
 
       // Conflux
       for (let i = 112; i < 126; i++) {
-        this.data.units.list.conflux.push(unitsJson[i])
+        this.units.conflux.push(unitsJson[i])
       }
 
       // Cove
       for (let i = 141; i < 156; i++) {
-        this.data.units.list.cove.push(unitsJson[i])
+        this.units.cove.push(unitsJson[i])
       }
 
       // Neutral
       for (let i = 126; i < 141; i++) {
-        this.data.units.list.neutral.push(unitsJson[i])
+        this.units.neutral.push(unitsJson[i])
       }
 
       // Neutral
       for (let i = 156; i < 161; i++) {
-        this.data.units.list.neutral.push(unitsJson[i])
+        this.units.neutral.push(unitsJson[i])
       }
     }
   }
--- src/components/damageCalculator/selectHero.vue
+++ src/components/damageCalculator/selectHero.vue
@@ -1,12 +1,21 @@
 <template lang='pug'>
 multiselect(
-  v-model='hero'
-  :options='heroes'
+  placeholder='Pick hero',
+  v-model='hero',
+  :value='hero',
+  :options='heroes',
   :searchable='true',
+  :allow-empty='false',
   label='name',
-  placeholder='Pick hero'
-  @input='setHero({ side, hero })'
 )
+  template(slot="singleLabel", slot-scope="props" v-if='hero.id')
+    img.option__image(:src='heroesImages[hero.id].src')
+    span.option__desc
+      span.option__title.ml-3 {{ props.option.name[0] }}
+  template(slot="option", slot-scope="props")
+    img.option__image(:src='heroesImages[props.option.id].src')
+    span.option__desc
+      span.option__title  {{ props.option.name[0] }}
 </template>
 
 <script>
@@ -17,15 +26,25 @@ export default {
   props: {
     side: String
   },
-  data () {
-    return {
-      hero: null
-    }
-  },
   computed: {
     ...mapGetters({
-      heroes: 'calculator/getHeroes'
-    })
+      heroes: 'calculator/getHeroes',
+      heroesImages: 'calculator/getHeroesImages'
+    }),
+    hero: {
+      get () {
+        if (this.side === 'attacker') {
+          return this.$store.state.calculator.attacker.hero
+        } else if (this.side === 'defender') {
+          return this.$store.state.calculator.defender.hero
+        } else {
+          return null
+        }
+      },
+      set (value) {
+        this.setHero({ side: this.side, hero: value })
+      }
+    }
   },
   methods: {
     ...mapActions({
