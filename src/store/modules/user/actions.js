5e05cc746b4750cf6a7ccb04d38d948637d4a0c0
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2019-03-26 15:00:27 +0500

    fix localstorage, remove unused code

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,7 +1,4 @@
 export default {
-  updateScreenResolution ({ commit }, { width, height }) {
-    commit('UPDATE_SCREEN_RESOLUTION', { width, height })
-  },
   setLocale ({ commit }, locale) {
     commit('SET_LOCALE', locale)
   }
