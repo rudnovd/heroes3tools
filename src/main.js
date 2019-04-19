00ecbfa79db7a0d30ba00efa4ea58cbbf4e20da0
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2019-04-19 13:55:19 +0500

    Added sentry, updated gitignore

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import i18n from './i18n'
 
 import BootstrapVue from 'bootstrap-vue'
 
+import * as Sentry from '@sentry/browser'
+
+import * as Integrations from '@sentry/integrations'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import 'bootstrap-vue/dist/bootstrap-vue.min.css'
@@ -23,3 +27,15 @@ new Vue({
   i18n,
   render: h => h(App)
 }).$mount('#app')
+
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new Integrations.Vue({
+        Vue,
+        attachProps: true
+      })
+    ]
+  })
+}
