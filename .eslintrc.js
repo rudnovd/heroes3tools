36d7b9652d047c641bcb3f2fb7ed20905f82b5cd
rudnovd <rudnov.d@gmail.com>
2020-11-21 21:45:35 +0500

    feat: support Typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,32 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    '@vue/typescript',
     'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    ecmaVersion: 2015,
+    parser: '@typescript-eslint/parser',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
