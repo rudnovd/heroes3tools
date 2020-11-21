c39b73d7d1b97f88da6c8ce5b92d06ca4450d8b7
rudnovd <rudnov.d@gmail.com>
2020-11-21 21:43:41 +0500

    chore: update root configs

---
+++ commitlint.js
@@ -0,0 +1,20 @@
+// https://commitlint.js.org/#/reference-rules
+
+module.exports = {
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [0, 'always'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-case': [0, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+  },
+}
