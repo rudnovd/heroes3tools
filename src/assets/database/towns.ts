c78100af160b403f091bd03d31d3707fb0a0d9b1
rudnovd <rudnov.d@gmail.com>
2020-12-06 15:54:53 +0500

    feat(assets): init database assets

---
+++ src/assets/database/towns.ts
@@ -0,0 +1,48 @@
+import { Town } from '@/models/Town'
+
+export const towns: Array<Town> = [
+  {
+    id: 1,
+    name: 'Castle',
+  },
+  {
+    id: 2,
+    name: 'Rampart',
+  },
+  {
+    id: 3,
+    name: 'Tower',
+  },
+  {
+    id: 4,
+    name: 'Inferno',
+  },
+  {
+    id: 5,
+    name: 'Necropolis',
+  },
+  {
+    id: 6,
+    name: 'Dungeon',
+  },
+  {
+    id: 7,
+    name: 'Stronghold',
+  },
+  {
+    id: 8,
+    name: 'Fortress',
+  },
+  {
+    id: 9,
+    name: 'Conflux',
+  },
+  {
+    id: 10,
+    name: 'Cove',
+  },
+  {
+    id: 11,
+    name: 'Factory',
+  },
+]
