cd1154578af85c23d3e7aad7bdef641edf259b4f
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2020-12-06 15:54:53 +0500

    feat(assets): init database assets

---
+++ src/assets/database/locales/ru/towns.ts
@@ -0,0 +1,48 @@
+import { TownTranslation } from '@/models/Town'
+
+export const towns: Array<TownTranslation> = [
+  {
+    id: 1,
+    name: 'Замок',
+  },
+  {
+    id: 2,
+    name: 'Оплот',
+  },
+  {
+    id: 3,
+    name: 'Башня',
+  },
+  {
+    id: 4,
+    name: 'Инферно',
+  },
+  {
+    id: 5,
+    name: 'Некрополис',
+  },
+  {
+    id: 6,
+    name: 'Темница',
+  },
+  {
+    id: 7,
+    name: 'Цитадель',
+  },
+  {
+    id: 8,
+    name: 'Крепость',
+  },
+  {
+    id: 9,
+    name: 'Сопряжение',
+  },
+  {
+    id: 10,
+    name: 'Причал',
+  },
+  {
+    id: 11,
+    name: 'Фабрика',
+  },
+]
