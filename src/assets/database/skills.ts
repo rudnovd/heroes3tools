1d2d13f184e8a7512077f328d79dfef85c6f48cb
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2020-12-30 20:48:01 +0500

    feat(database): add new data (levels, skills, spells)

---
+++ src/assets/database/skills.ts
@@ -0,0 +1,36 @@
+import { Skill } from '@/models/Skill'
+
+export const skills: Array<Skill> = [
+  {
+    id: 1,
+    name: 'Offense',
+  },
+  {
+    id: 2,
+    name: 'Air',
+  },
+  {
+    id: 3,
+    name: 'Armorer',
+  },
+  {
+    id: 4,
+    name: 'Fire',
+  },
+  {
+    id: 5,
+    name: 'Archery',
+  },
+  {
+    id: 6,
+    name: 'Earth',
+  },
+  {
+    id: 7,
+    name: 'Artillery',
+  },
+  {
+    id: 7,
+    name: 'Water',
+  },
+]
