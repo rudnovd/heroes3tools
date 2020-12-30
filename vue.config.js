d1bc5b4680b4b7e2fac7409943e6cf37919feb2e
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2020-12-30 21:08:46 +0500

    feat: add service worker and web manifest

--- vue.config.js
+++ vue.config.js
@@ -23,4 +23,43 @@ module.exports = {
       enableInSFC: false,
     },
   },
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Heroes 3 tools'
+      return args
+    })
+  },
+  pwa: {
+    name: 'Heroes 3 tools',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      author: 'https://github.com/rudnovd',
+      developer: {
+        name: 'rudnovd',
+        url: 'https://github.com/rudnovd',
+      },
+      manifest_version: 2,
+      version: '0.1',
+      background_color: '#42B883',
+      icons: [
+        { src: 'favicon-192.png', type: 'image/png', sizes: '192x192' },
+        { src: 'favicon-512.png', type: 'image/png', sizes: '512x512' },
+      ],
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
+    iconPaths: {
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+  },
 }
