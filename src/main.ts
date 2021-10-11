c195063e14c8b4d87935c9de7110a8bd06ecf4a0
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2021-10-11 14:18:28 +0500

    feat: init pwa

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
 import App from '@/App.vue'
 import i18n from '@/i18n'
 import router from '@/router'
 import store from '@/store'
-import '@/registerServiceWorker'
+import { createApp } from 'vue'
 
 createApp(App).use(i18n).use(router).use(store).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      mode: 'development',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'favicon-apple.png',
+        'favicon-192.png',
+        'favicon-512.png',
+      ],
+      manifest: {
+        name: 'Heroes 3 tools',
+        short_name: 'heroes3tools',
+        description: 'Web tools for simplification playing in Heroes of Might and Magic III: Horn of The Abyss',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        sourcemap: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
