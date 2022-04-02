2227df0bb5503b3878d0b216e78a844efd815855
rudnovd <rudnov.d@gmail.com>
2022-04-02 16:07:32 +0500

    test: add creature special modificator tests

---
+++ tests/unit/modificators/creatureSpecial.spec.ts
@@ -0,0 +1,60 @@
+import { Battle } from '@/models/Battle'
+import { Creatures } from '@/models/enums'
+import { beforeEach, describe, expect, test } from 'vitest'
+import { getCreatureInstance } from '../helpers'
+
+describe('Creature special modificator', () => {
+  let battle: Battle
+
+  beforeEach(() => {
+    battle = new Battle()
+  })
+
+  test('Behemoth vs Angel', () => {
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Behemoth)
+    battle.attacker.activeCreature.count = 1
+
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Angel)
+    battle.defender.activeCreature.count = 1
+
+    const { attacker } = battle.calculate()
+
+    expect(attacker.activeCreature.calculation).toContain({ minDamage: 39, maxDamage: 65 })
+  })
+
+  test('Ancient Behemoth vs Angel', () => {
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.AncientBehemoth)
+    battle.attacker.activeCreature.count = 1
+
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Angel)
+    battle.defender.activeCreature.count = 1
+
+    const { attacker } = battle.calculate()
+
+    expect(attacker.activeCreature.calculation).toContain({ minDamage: 54, maxDamage: 90 })
+  })
+
+  test('Angel vs Nix', () => {
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Angel)
+    battle.attacker.activeCreature.count = 1
+
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Nix)
+    battle.defender.activeCreature.count = 1
+
+    const { attacker } = battle.calculate()
+
+    expect(attacker.activeCreature.calculation).toContain({ minDamage: 47, maxDamage: 47 })
+  })
+
+  test('Angel vs Nix Warrior', () => {
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Angel)
+    battle.attacker.activeCreature.count = 1
+
+    battle.defender.activeCreature = getCreatureInstance(Creatures.NixWarrior)
+    battle.defender.activeCreature.count = 1
+
+    const { attacker } = battle.calculate()
+
+    expect(attacker.activeCreature.calculation).toContain({ minDamage: 38, maxDamage: 38 })
+  })
+})
