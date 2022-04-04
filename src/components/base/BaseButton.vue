b18819e147e0ff408c51bf32162b763b276f2046
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2022-04-04 12:43:49 +0500

    feat(components): add BaseButton component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button class="base-button" :class="`color-${color}`" @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import type { BattleSide } from '@/models/Battle'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'BaseButton',
+  props: {
+    color: {
+      type: String as PropType<BattleSide>,
+      default: 'attacker',
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+  },
+  emits: ['click'],
+})
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  width: v-bind(width);
+  font-size: 0.875rem;
+  font-weight: bold;
+  line-height: 1.5;
+  border-radius: 5px;
+  transition: background-color 0.25s linear, border-color 0.25s linear;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+@each $color, $color-value in $calculator-colors {
+  .color-#{$color} {
+    color: map.get($calculator-colors-text, $color);
+    background-color: $color-value;
+    border: 1px solid $color-value;
+
+    &:hover {
+      background-color: color.adjust($color-value, $red: -10, $green: -10, $blue: -10);
+      border-color: color.adjust($color-value, $red: -10, $green: -10, $blue: -10);
+    }
+  }
+}
+</style>
