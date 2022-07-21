628b7caf6102b0403f919fe03fa90d3415659c5c
rudnovd <rudnov.d@gmail.com>
2022-07-21 22:21:41 +0500

    fix(App): remove announce about new version

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,4 @@
 <template>
-  <div v-if="route.path === '/damage' && route.query.from && showAnnounce" class="announce-header">
-    <div class="announce-text">
-      <i18n-t keypath="common.newCalculatorAnnounce.announce.text" tag="span">
-        <template #whatsNew>
-          <router-link to="#whats-new">{{ t('common.newCalculatorAnnounce.announce.whatsNew') }}</router-link>
-        </template>
-        <template #here>
-          <a href="https://old.heroes3.tools">{{ t('common.newCalculatorAnnounce.announce.here') }}</a>
-        </template>
-      </i18n-t>
-    </div>
-    <CloseButton @click="showAnnounce = false" />
-  </div>
-
   <router-link v-if="showBackButton" class="return-home" to="/">
     <img src="@/assets/icons/arrow_back.svg" alt="back" width="16" height="16" />
     {{ t('common.homePage') }}
@@ -27,72 +13,6 @@
   <BaseNotification v-if="needRefresh" :show="needRefresh" :buttons="notificationsButtons">
     {{ t('common.newContentIsAvailable') }}.
   </BaseNotification>
-
-  <BaseDialog
-    v-if="route.hash === '#whats-new'"
-    :show="route.hash === '#whats-new'"
-    @close="router.replace(route.path)"
-  >
-    <template #content>
-      <div class="whats-new-modal-content">
-        <div class="content-step">
-          <template v-if="whatsNewStep === 1">
-            <span>
-              <strong>{{ t('common.newCalculatorAnnounce.modal.steps.1.title') }}</strong>
-              {{ t('common.newCalculatorAnnounce.modal.steps.1.text') }}
-            </span>
-            <img
-              src="/images/windows/damage-calculator-whats-new/1-damage-calculator-tabs.webp"
-              alt="damage-calculator-tabs"
-              :width="720"
-            />
-          </template>
-
-          <template v-if="whatsNewStep === 2">
-            <span>
-              <strong>{{ t('common.newCalculatorAnnounce.modal.steps.2.title') }}</strong>
-              {{ t('common.newCalculatorAnnounce.modal.steps.2.text') }}
-            </span>
-            <img
-              src="/images/windows/damage-calculator-whats-new/2-install-app.webp"
-              :height="252"
-              :width="389"
-              alt="install-app"
-            />
-          </template>
-
-          <template v-if="whatsNewStep === 3">
-            <p>
-              <strong>{{ t('common.newCalculatorAnnounce.modal.steps.3.title') }}</strong>
-              {{ t('common.newCalculatorAnnounce.modal.steps.3.text') }}
-            </p>
-            <p>
-              <i18n-t keypath="common.newCalculatorAnnounce.modal.steps.3.oldVersionText" tag="span">
-                <template #here>
-                  <a href="https://old.heroes3.tools">{{ t('common.newCalculatorAnnounce.announce.here') }}</a>
-                </template>
-              </i18n-t>
-            </p>
-
-            <p>
-              <BaseButton width="250px" @click="router.replace(route.path)">
-                {{ t('common.newCalculatorAnnounce.modal.steps.3.startCalculating') }}
-              </BaseButton>
-            </p>
-          </template>
-        </div>
-
-        <div class="modal-buttons">
-          <BaseButton v-if="whatsNewStep > 1" color="defender" @click="whatsNewStep--">
-            {{ t('common.newCalculatorAnnounce.modal.previous') }}
-          </BaseButton>
-          <BaseButton v-if="whatsNewStep < 3" @click="whatsNewStep++">
-            {{ t('common.newCalculatorAnnounce.modal.next') }}
-          </BaseButton>
-        </div>
-      </div>
-    </template>
-  </BaseDialog>
 </template>
 
 <script lang="ts">
@@ -107,9 +27,6 @@ export default defineComponent({
   name: 'App',
   components: {
     BaseNotification: defineAsyncComponent(() => import('@/components/base/BaseNotification.vue')),
-    CloseButton: defineAsyncComponent(() => import('@/components/buttons/CloseButton.vue')),
-    BaseDialog: defineAsyncComponent(() => import('@/components/base/BaseDialog.vue')),
-    BaseButton: defineAsyncComponent(() => import('@/components/base/BaseButton.vue')),
   },
   setup() {
     const router = useRouter()
@@ -117,8 +34,6 @@ export default defineComponent({
     const { t } = useI18n()
 
     const showBackButton = ref(false)
-    const showAnnounce = ref(true)
-    const whatsNewStep = ref(1)
 
     const { updateServiceWorker, needRefresh } = useRegisterSW({
       immediate: false,
@@ -172,14 +87,10 @@ export default defineComponent({
 
     return {
       t,
-      router,
-      route,
 
       showBackButton,
       needRefresh,
       notificationsButtons,
-      showAnnounce,
-      whatsNewStep,
 
       onEnter,
       afterLeave,
@@ -212,41 +123,6 @@ export default defineComponent({
     width: 2rem;
     height: 2rem;
     background: url('@/assets/icons/arrow_back.svg');
-  }
-}
-
-.announce-header {
-  display: grid;
-  grid-template-columns: 1fr auto;
-  align-items: center;
-  padding: 0 4px;
-  font-size: 12px;
-  font-weight: 600;
-  background-color: $color-attacker;
-
-  .announce-text {
-    display: flex;
-    justify-content: center;
-  }
-
-  & > button {
-    width: 12px;
-    height: 12px;
-  }
-}
-
-.whats-new-modal-content {
-  .content-step {
-    display: grid;
-    gap: 16px;
-    margin-bottom: 16px;
-  }
-  .modal-buttons {
-    display: grid;
-    grid-auto-columns: 100px;
-    grid-auto-flow: column;
-    gap: 8px;
-    justify-content: center;
   }
 }
 
