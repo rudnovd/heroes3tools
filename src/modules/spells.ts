ab46f1438b0ace348215d8a98200c0cfc71dbb03
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2022-08-04 18:59:49 +0500

    refactor(spells): move spells function in Battle

--- src/modules/spells.ts
+++ src/modules/spells.ts
@@ -1,113 +1,7 @@
-import { spells } from '@/assets/database/spells'
 import type { DamageCalculatorBattleSide } from '@/models/Battle'
-import type { CreatureInstance } from '@/models/Creature'
-import { Creatures, SkillLevels, Spells as SpellsEnum } from '@/models/enums'
-import type { Spell } from '@/models/Spell'
-
-function hasImmunity(target: CreatureInstance, spell: Spell) {
-  if (target.effects.find((effect) => effect.id === SpellsEnum.AntiMagic)) return true
-
-  const creaturesWithImmunity = [
-    Creatures.BlackDragon,
-    Creatures.MagicElemental,
-    Creatures.FireElemental,
-    Creatures.EnergyElemental,
-  ]
-  if (creaturesWithImmunity.includes(target.id)) return true
-
-  const fireImmunityCreatures = [Creatures.Efreet, Creatures.EfreetSultan]
-  if (fireImmunityCreatures.includes(target.id)) return true
-
-  const threeSpellLevelImmunityCreatures = [Creatures.GreenDragon, Creatures.RedDragon]
-  if (threeSpellLevelImmunityCreatures.includes(target.id) && spell.level <= 3) return true
-
-  const fourSpellLevelImmunityCreatures = [Creatures.GoldDragon]
-  if (fourSpellLevelImmunityCreatures.includes(target.id) && spell.level <= 4) return true
-
-  if (
-    (target.id === Creatures.AirElemental || target.id === Creatures.StormElemental) &&
-    spell.id === SpellsEnum.MeteorShower
-  )
-    return true
-
-  if (
-    (target.id === Creatures.WaterElemental || target.id === Creatures.IceElemental) &&
-    (spell.id === SpellsEnum.IceBolt || spell.id === SpellsEnum.FrostRing)
-  )
-    return true
-
-  if (
-    (target.id === Creatures.EarthElemental || target.id === Creatures.MagmaElemental) &&
-    (spell.id === SpellsEnum.LightningBolt ||
-      spell.id === SpellsEnum.ChainLightning ||
-      spell.id === SpellsEnum.Armageddon)
-  )
-    return true
-
-  return false
-}
-
-function calculateReducing(
-  initiator: DamageCalculatorBattleSide,
-  target: CreatureInstance,
-  spell: Spell,
-  damage: number
-) {
-  if (target.id === Creatures.StoneGolem) damage -= (damage / 100) * 50
-  else if (target.id === Creatures.IronGolem) damage -= (damage / 100) * 75
-  else if (target.id === Creatures.SteelGolem) damage -= (damage / 100) * 80
-  else if (target.id === Creatures.GoldGolem) damage -= (damage / 100) * 85
-  else if (target.id === Creatures.DiamondGolem) damage -= (damage / 100) * 95
-
-  if (spell.element === 'Fire' && target.effects.find((effect) => effect.id === SpellsEnum.ProtectionFromFire)) {
-    if (!initiator.hero) {
-      damage -= (damage / 100) * 30
-    } else if (initiator.hero.skills.fire && initiator.hero.skills.fire >= 1) {
-      damage -= (damage / 100) * 50
-    }
-  }
-
-  return damage
-}
-
-function calculateSpecialtyBonus(
-  initiator: DamageCalculatorBattleSide,
-  target: CreatureInstance,
-  spell: Spell,
-  damage: number
-) {
-  if (!initiator.hero) return damage
-
-  const { specialtySpell } = initiator.hero
-  let bonus = 0
-
-  if (spell.id === SpellsEnum.MagicArrow && specialtySpell === SpellsEnum.MagicArrow) {
-    bonus = damage / 2
-  } else if (spell.id === SpellsEnum.Firewall && specialtySpell === SpellsEnum.Firewall) {
-    bonus = damage
-  } else if (spell.id === specialtySpell) {
-    bonus = 1 + (initiator.hero.level / target.level) * 0.03
-  }
-
-  return damage + bonus
-}
+import { SkillLevels, Spells as SpellsEnum } from '@/models/enums'
 
 export const Spells = {
-  cast(initiator: DamageCalculatorBattleSide, target: CreatureInstance, spell: Spell) {
-    if (hasImmunity(target, spell)) return 0
-
-    const originalSpell = spells.find((_spell) => _spell.id === spell.id)!
-    const spellCamelName =
-      originalSpell.name[0].toLocaleLowerCase() +
-      originalSpell.name.slice(1, originalSpell.name.length).replace(/\s+/g, '')
-
-    let damage = this[spellCamelName](initiator)
-    damage = calculateSpecialtyBonus(initiator, target, spell, damage)
-    damage = calculateReducing(initiator, target, spell, damage)
-
-    return damage
-  },
-
   magicArrow: (initiator: DamageCalculatorBattleSide): number => {
     let damage = 0
 
@@ -345,7 +239,7 @@ export const Spells = {
     return damage
   },
 
-  titansLightningBolt: (initiator: DamageCalculatorBattleSide): number => {
+  titansLightningBolt: (): number => {
     return 600
   },
 
@@ -375,13 +269,34 @@ export const Spells = {
     if (!initiator.hero) {
       damage = 30
     } else if (schoolLevel <= SkillLevels.Basic) {
-      damage = 30 + initiator.hero.stats.power * 50
+      damage = 30 + initiator.hero.stats.power * 40
     } else if (schoolLevel <= SkillLevels.Advanced) {
-      damage = 60 + initiator.hero.stats.power * 50
+      damage = 60 + initiator.hero.stats.power * 40
     } else if (schoolLevel <= SkillLevels.Expert) {
-      damage = 120 + initiator.hero.stats.power * 50
+      damage = 120 + initiator.hero.stats.power * 40
     }
 
     return damage
   },
+}
+
+export const spellsFunctionsMap = {
+  [SpellsEnum.MagicArrow]: Spells.magicArrow,
+  [SpellsEnum.Firewall]: Spells.fireWall,
+  [SpellsEnum.Fireball]: Spells.fireball,
+  [SpellsEnum.LandMine]: Spells.landMine,
+  [SpellsEnum.Armageddon]: Spells.armageddon,
+  [SpellsEnum.Inferno]: Spells.inferno,
+  [SpellsEnum.Cure]: Spells.cure,
+  [SpellsEnum.IceBolt]: Spells.iceBolt,
+  [SpellsEnum.FrostRing]: Spells.frostRing,
+  [SpellsEnum.DeathRipple]: Spells.deathRipple,
+  // [SpellsEnum.AnimateDead]: Spells.animateDead,
+  [SpellsEnum.MeteorShower]: Spells.meteorShower,
+  // [SpellsEnum.Resurrection]: Spells.ressurection,
+  [SpellsEnum.Implosion]: Spells.implosion,
+  [SpellsEnum.LightningBolt]: Spells.lightningBolt,
+  [SpellsEnum.DestroyUndead]: Spells.destroyUndead,
+  [SpellsEnum.ChainLightning]: Spells.chainLightning,
+  [SpellsEnum.TitansLightningBolt]: Spells.titansLightningBolt,
 }
