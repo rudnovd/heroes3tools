117bc3cac82d4a5315d8715b41e16220cb93ebeb
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2022-08-07 16:56:23 +0500

    feat(models): update creatures specials interface
    add new props, refactor and fix old props

--- src/models/CreaturesSpecials.ts
+++ src/models/CreaturesSpecials.ts
@@ -1,5 +1,3 @@
-import type { Spell } from '@/models/Spell'
-
 export type CreaturesSpecial = Readonly<
   Partial<{
     // Creature hit twice
@@ -21,12 +19,15 @@ export type CreaturesSpecial = Readonly<
     // Melee Attack special for ranged creatures
     noMeleePenalty: boolean
 
+    // No penalty when target behind walls
+    noObstaclePenalty: boolean
+
     // Moving before the attack will increase their damage by 5% for each hex
     // Cavalier and Champion attack special
     jousting: boolean
 
     // Unicorn and War Unicorn special effect
-    blindChange: number
+    blindChance: number
 
     // Wyvern Monarch, Sea Serpent, Haspid special effect
     poisonChance: number
@@ -53,8 +54,8 @@ export type CreaturesSpecial = Readonly<
     // Mummy, Black Knight, Dread Knight special effect
     curseChance: number
 
-    // Meduza and Basilisk special effect
-    petrifyChance: number
+    // Meduza and Basilisk special effect (always 20%)
+    petrify: boolean
 
     // Vampire Lord special effect
     lifeDrain: boolean
@@ -62,17 +63,37 @@ export type CreaturesSpecial = Readonly<
     // Ghost Dragon special effect
     agingChance: number
 
+    immunityToJousting: boolean
+
     // Spell immunity
-    immunity: Array<Spell>
+    immunity: Array<number>
+
+    immunityToSpellLevels: Array<number>
+
+    immunityToSpellElement: Array<string>
+
+    vulnerablesToSpells: Array<number>
 
     // Resistance to all spells chance
     magicResistanceChance: number
 
+    auraOfMagicResistanceChance: number
+
+    reduceCastSpellPointsCount: number
+
     // Percents reduces damage from spells
     spellDamageResistance: number
 
+    ranged: boolean
+
+    shots: number
+
     // Is creature don't use terrain for moving
     flying: boolean
+
+    dragon: boolean
+
+    breathAttack: boolean
 
     undead: boolean
     nonLiving: boolean
