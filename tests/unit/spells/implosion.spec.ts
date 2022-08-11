53e33b0dd2bfc334484b14aa512fe54b6d5dd835
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2022-08-11 17:37:52 +0500

    test: add new spells tests

---
+++ tests/unit/spells/implosion.spec.ts
@@ -0,0 +1,113 @@
+import { spells } from '@/assets/database/spells'
+import { Battle } from '@/models/Battle'
+import { Creatures, Heroes, Spells as SpellsEnum } from '@/models/enums'
+
+import { beforeEach, describe, expect, test } from 'vitest'
+import { getCreatureInstance, getHeroInstance } from '../helpers'
+
+describe('Implosion', () => {
+  let battle: Battle
+  const implosion = spells.find((spell) => spell.id === SpellsEnum.Implosion)!
+
+  beforeEach(() => {
+    battle = new Battle()
+  })
+
+  test('Base spell values', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 0
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(100)
+  })
+
+  test('With Spell Power', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 10
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(850)
+  })
+
+  test('With Advanced Earth skill', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 0
+    battle.attacker.hero.skills.earth = 2
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(200)
+  })
+
+  test('With Advanced Earth skill and high Spell Power', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 30
+    battle.attacker.hero.skills.earth = 2
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(2450)
+  })
+
+  test('With Expert Earth skill', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 0
+    battle.attacker.hero.skills.earth = 3
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(300)
+  })
+
+  test('With Expert Earth skill and high Spell Power', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 30
+    battle.attacker.hero.skills.earth = 3
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+    battle.defender.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const damage = battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+
+    expect(damage).toBe(2550)
+  })
+
+  test('Cast on creature with immunity', () => {
+    battle.attacker.hero = getHeroInstance(Heroes.Orrin)
+    battle.attacker.hero.stats.power = 0
+
+    battle.attacker.activeCreature = getCreatureInstance(Creatures.Pikeman)
+
+    const creaturesWithImmunity = [
+      Creatures.BlackDragon,
+      Creatures.MagicElemental,
+      Creatures.Ballista,
+      Creatures.Cannon,
+    ]
+
+    let totalDamage = 0
+    creaturesWithImmunity.forEach((creature) => {
+      battle.defender.activeCreature = getCreatureInstance(creature)
+      totalDamage += battle.cast(battle.attacker, battle.defender, battle.defender.activeCreature, implosion)
+    })
+
+    expect(totalDamage).toBe(0)
+  })
+})
