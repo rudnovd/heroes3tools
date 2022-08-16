69e7f74c7f587decc66f555b870f081f0a5748ba
rudnovd <rudnov.d@gmail.com>
2022-08-16 12:25:32 +0500

    fix(effects): fix Bless effect

--- src/modules/effects.ts
+++ src/modules/effects.ts
@@ -67,7 +67,7 @@ export const Effects = {
 
     // if battle side has hero with Bless specialty then add damageBonus value
     if (initiator.hero && initiator.hero.specialtySpell === Spells.Bless) {
-      damageBonus += (0.03 * initiator.hero.level) / target.level
+      damageBonus += Math.floor(initiator.hero.level / target.level) * 0.03
     }
 
     if (!initiator.hero || !initiator.hero.skills.water || initiator.hero.skills.water <= 1) {
