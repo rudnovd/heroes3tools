a100e5b53a53deaaf69b990d33b141b0f9bdb16f
rudnovd <rudnov.d@gmail.com>
2022-09-03 23:26:03 +0500

    refactor(HomePage): refactor component

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,144 +1,91 @@
 <template>
-  <section class="home-page">
+  <main>
     <h1>Heroes III tools</h1>
 
-    <main class="page-links">
-      <div v-for="page in pages" :key="page.path" class="link-card">
-        <ObjectPortrait
-          folder="/images/pages/home"
-          :file="{ name: page.image, alt: page.image }"
-          :width="176"
-          :height="198"
-          @click-picture="router.push(page.path)"
-        />
+    <section>
+      <article v-for="page in pages" :key="page.link">
+        <picture @click="router.push(page.link)">
+          <img :src="`${page.image}.webp`" :alt="t(page.name)" width="176" height="198" />
+        </picture>
 
         <h2>
-          <router-link :to="page.path">{{ page.name }}</router-link>
+          <router-link v-t="page.name" :to="page.link" />
         </h2>
-      </div>
-    </main>
-
-    <PageFooter>
-      <template #aboutModal>
-        <h2>{{ t('components.homePage.data.1') }}</h2>
-        <p>{{ t('components.homePage.data.2') }}</p>
-
-        <i18n-t keypath="components.homePage.data.3" tag="p">
-          <template #email>
-            <a href="mailto:feedback@heroes3.tools" target="_blank">feedback@heroes3.tools</a>
-          </template>
-        </i18n-t>
-      </template>
-    </PageFooter>
-  </section>
+      </article>
+    </section>
+  </main>
 </template>
 
-<script lang="ts">
-import { computed, defineAsyncComponent, defineComponent } from 'vue'
+<script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 
-export default defineComponent({
-  name: 'HomePage',
-  components: {
-    ObjectPortrait: defineAsyncComponent(() => import('@/components/ObjectPortrait.vue')),
-    PageFooter: defineAsyncComponent(() => import('@/components/PageFooter.vue')),
+const router = useRouter()
+const { t } = useI18n()
+
+const pages = [
+  {
+    name: 'pages.damageCalculator',
+    link: '/damage',
+    image: '/secondary-skills/Offense-Expert_upscayled',
   },
-  setup() {
-    const { t } = useI18n()
-    const router = useRouter()
-
-    const pages = computed(() => [
-      {
-        name: t('pages.damageCalculator'),
-        path: '/damage',
-        image: 'Tactics',
-      },
-      {
-        name: t('pages.magicCalculator'),
-        path: '/magic',
-        image: 'Water',
-      },
-      {
-        name: t('pages.creaturesLibrary'),
-        path: '/creatures',
-        image: 'Scholar',
-      },
-    ])
-
-    return {
-      t,
-      router,
-
-      pages,
-    }
+  {
+    name: 'pages.magicCalculator',
+    link: '/magic',
+    image: '/secondary-skills/FireMagic-Expert_upscayled',
   },
-})
+  {
+    name: 'pages.creaturesLibrary',
+    link: '/creatures',
+    image: '/secondary-skills/Scholar-Expert_upscayled',
+  },
+]
 </script>
 
 <style lang="scss" scoped>
-.home-page {
+main {
   display: grid;
-  grid-template-rows: min-content 1fr min-content;
-  gap: 16px;
-  min-width: 300px;
-  min-height: 100vh;
-  margin: 0 auto;
-  content-visibility: auto;
-  contain-intrinsic-size: 100vh;
-
-  @include media-medium {
-    align-content: center;
-    padding-bottom: unset;
-  }
-
-  @include media-large {
-    grid-template-rows: 1fr 2fr 30px;
-  }
-
-  & > h1 {
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-  }
-}
-
-.page-links {
-  display: grid;
-  grid-template-columns: 100%;
-  grid-auto-rows: min-content;
-  gap: 1rem;
+  grid-template-rows: min-content 1fr;
+  gap: 2rem;
   align-items: start;
+  padding: 0 16px 16px;
+}
+
+h1 {
+  text-align: center;
+}
+
+section {
+  display: grid;
+  gap: 1rem;
   justify-items: center;
-  padding: 0px 16px 16px;
 
   @include media-medium {
-    grid-template-columns: repeat(2, minmax(300px, 1fr));
-    justify-content: center;
+    grid-template-columns: repeat(2, 1fr);
   }
 
   @include media-large {
-    grid-template-columns: repeat(3, minmax(300px, 400px));
-    gap: 3rem;
-    padding: 0;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
   }
 }
 
-.link-card {
+article {
   display: grid;
   grid-template-rows: 200px 3rem;
+  grid-auto-columns: 100%;
   width: 100%;
   max-width: 400px;
-  user-select: none;
-  box-shadow: 0 0 8px black;
-  transition: filter 1s, box-shadow 1s, transform 1s;
+  border: 1px solid rgb(0, 0, 0);
+  box-shadow: 0 0 8px rgb(0, 0, 0);
+  transition: filter 1s ease-out, box-shadow 1s ease-out, transform 1s ease-out;
 
   picture {
     display: flex;
     align-items: center;
     justify-content: center;
-    background: url('/images/pages/home/links-background.webp');
-    background-position-y: -1px;
+    user-select: none;
+    background: url('/backgrounds/base-window.webp');
 
     &:hover {
       cursor: pointer;
@@ -149,28 +96,21 @@ export default defineComponent({
     display: flex;
     align-items: center;
     justify-content: center;
-    overflow: hidden;
-    transition: transform 0.15s linear;
+    padding: 0 0.5rem;
   }
 
-  a,
-  span {
+  a {
     overflow: hidden;
     color: var(--color-text);
     text-decoration: none;
     text-overflow: ellipsis;
     white-space: nowrap;
-    transition: transform 0.15s linear;
   }
 
   &:hover {
     filter: contrast(110%);
-    box-shadow: 0 0 20px black;
-    transform: translateY(-5px);
-
-    h2 {
-      transform: translateY(-2px);
-    }
+    box-shadow: 0 0 20px rgb(0, 0, 0);
+    transform: translateY(-8px);
 
     a {
       text-decoration: underline;
