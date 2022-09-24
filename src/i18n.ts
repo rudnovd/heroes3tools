6e24eb6f229f7d7c9d132848f7d242333860a6f9
rudnovd <rudnov.d@gmail.com>
2022-09-24 19:26:30 +0500

    refactor(i18n): update initial locale, add new type

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,11 @@
 import { createI18n } from 'vue-i18n'
 
-export const availableLocales: Readonly<Array<string>> = ['en', 'ru']
+export type AvailableLocale = 'en' | 'ru'
+export const availableLocales: Readonly<Array<AvailableLocale>> = ['en', 'ru']
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'en',
-  availableLocales,
+  locale: localStorage.getItem('locale') || 'en',
 })
 
 export async function loadLocaleMessages(locale: string) {
@@ -16,7 +16,7 @@ export function getBrowserLanguage() {
   if (!navigator.language) return 'en'
 
   // get first language code (e.g. get 'en' from en-US)
-  const navigatorLanguage = navigator.language.trim().split(/-|_/)[0]
+  const navigatorLanguage = navigator.language.trim().split(/-|_/)[0] as AvailableLocale
   if (availableLocales.includes(navigatorLanguage)) {
     return navigatorLanguage
   } else {
