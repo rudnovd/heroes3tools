417aadbc1e5805da61c08269ab58b17ffd564e50
rudnovd <rudnov.d@gmail.com>
2022-09-26 22:01:34 +0500

    feat(store): update store functions
    move pinia creation from main.ts to store.ts;
    
    add artifacts array to the
    state;
    
    move getters to DamageCalculatorPage components;
    
    update
    actions:
    
    loadData function saves data from @/assets/data to indexedDB,
    then sends it to state;
    
    loadLocaleData function saves locales data from
    @/assets/data/locales/...  to indexedDB, then merges it with the current
    data in state.

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,12 @@
-import { defineStore } from 'pinia'
-import type { Class } from './models/Class'
-import type { Creature } from './models/Creature'
-import { Spells } from './models/enums'
-import type { Hero } from './models/Hero'
-import type { Level } from './models/Level'
-import type { Skill } from './models/Skill'
-import type { Spell } from './models/Spell'
-import type { Terrain } from './models/Terrain'
-import type { Town } from './models/Town'
+import type { AvailableLocale } from '@/i18n'
+import type { Artifact, Creature, Hero, HeroClass, Level, Skill, Spell, Terrain, Town } from '@/types'
+import { dataModules } from '@/utilities'
+import { openDB } from 'idb'
+import { createPinia, defineStore } from 'pinia'
 
 export interface StoreState {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any
-  classes: Array<Class>
+  artifacts: Array<Artifact>
+  classes: Array<HeroClass>
   creatures: Array<Creature>
   heroes: Array<Hero>
   skills: Array<Skill>
@@ -24,8 +18,11 @@ export interface StoreState {
   isDataLoaded: boolean
 }
 
+const pinia = createPinia()
+
 export const useStore = defineStore('data', {
   state: (): StoreState => ({
+    artifacts: [],
     classes: [],
     creatures: [],
     heroes: [],
@@ -37,50 +34,96 @@ export const useStore = defineStore('data', {
 
     isDataLoaded: false,
   }),
-  getters: {
-    attackPositiveEffects(): Array<Spell> {
-      const attackPositiveEffectsIds = [
-        Spells.Bless,
-        Spells.Bloodlust,
-        Spells.Frenzy,
-        Spells.Prayer,
-        Spells.Precision,
-        Spells.Slayer,
-      ]
-      return this.spells.filter((spell) => attackPositiveEffectsIds.indexOf(spell.id) !== -1)
-    },
-    defensePositiveEffects(): Array<Spell> {
-      const defensePositiveEffectsIds = [Spells.Shield, Spells.StoneSkin, Spells.AirShield]
-      return this.spells.filter((spell) => defensePositiveEffectsIds.indexOf(spell.id) !== -1)
-    },
-    attackNegativeEffects(): Array<Spell> {
-      const attackNegativeEffectsIds = [Spells.Curse, Spells.Weakness, Spells.DisruptingRay]
-      return this.spells.filter((spell) => attackNegativeEffectsIds.indexOf(spell.id) !== -1)
-    },
-  },
   actions: {
-    loadData(locale = 'en') {
-      const tables = ['classes', 'creatures', 'heroes', 'skills', 'levels', 'terrains', 'spells', 'towns']
-
-      // dynamic load data modules
-      const promises = tables.map(async (table: string) => {
-        // load original data if this not exist in store
-        if (!this[table].length || locale === 'en') {
-          const originalData = await import(`./assets/database/${table}.ts`)
-          this[table] = originalData[table]
-        }
-        if (locale === 'en') return
-
-        // load translation data
-        const data = await import(`./assets/database/locales/${locale}/${table}.ts`)
-        this[table] = this[table].map((tableData: Record<string, unknown>, index: number) => {
-          return {
-            ...tableData,
-            ...data[table][index],
+    async loadData(locale: AvailableLocale) {
+      // open database
+      const database = await openDB('data', import.meta.env.VITE_APP_DATABASES_VERSIONS.DATA, {
+        // called if this version of the database has never been opened before
+        upgrade(db) {
+          // delete old stores when database version upgraded
+          for (const module of db.objectStoreNames) {
+            db.deleteObjectStore(module)
           }
-        })
+
+          // create stores
+          for (const module of dataModules) {
+            db.createObjectStore(module)
+          }
+        },
       })
-      Promise.all(promises).then(() => (this.isDataLoaded = true))
+
+      for (const store of database.objectStoreNames) {
+        // if data already exist - continue
+        if (await database.count(store)) continue
+
+        // save data in indexedDB from file
+        const module = await import(`@/assets/data/${store}.ts`)
+        module.default.forEach((value: object, index: number) => database.add(store, value, index))
+      }
+
+      try {
+        // fill store from database
+        for (const store of database.objectStoreNames) {
+          this[store] = await database.getAll(store)
+        }
+
+        // load locale data
+        if (locale !== 'en') {
+          this.loadLocaleData(locale)
+        }
+      } finally {
+        database.close()
+        if (locale === 'en') {
+          this.isDataLoaded = true
+        }
+      }
+    },
+    async loadLocaleData(locale: AvailableLocale) {
+      if (locale === 'en') {
+        return this.loadData('en')
+      }
+
+      this.isDataLoaded = false
+
+      // open locales database
+      const database = await openDB('locales', import.meta.env.VITE_APP_DATABASES_VERSIONS.LOCALES, {
+        // called if this version of the database has never been opened before
+        upgrade(db) {
+          // delete old stores when database version upgraded
+          for (const store of db.objectStoreNames) {
+            db.deleteObjectStore(store)
+          }
+
+          // create stores
+          db.createObjectStore(locale)
+        },
+      })
+
+      try {
+        for (const module of dataModules) {
+          let localeData: object = await database.get(locale, module)
+
+          // add data to the database, if it is unavailable
+          if (!localeData || !Object.keys(localeData).length) {
+            const data = await import(`@/assets/data/locales/${locale}/${module}.ts`)
+            database.add(locale, data.default, module)
+            localeData = data.default
+          }
+
+          // merge data in store
+          this[module] = this[module].map((value) => {
+            return {
+              ...value,
+              ...localeData[value.key],
+            }
+          })
+        }
+      } finally {
+        database.close()
+        this.isDataLoaded = true
+      }
     },
   },
 })
+
+export default pinia
