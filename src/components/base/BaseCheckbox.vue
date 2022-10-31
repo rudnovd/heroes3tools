ab92c4c1760fb652b184882949a4ccdd94da80f8
rudnovd <rudnov.d@gmail.com>
2022-10-31 22:37:06 +0500

    feat(BaseCheckbox): update component

--- src/components/base/BaseCheckbox.vue
+++ src/components/base/BaseCheckbox.vue
@@ -1,135 +1,92 @@
 <template>
-  <label class="check option" :class="[{ 'check-disabled': disabled }, `check-color-${color}`]">
+  <label class="checkbox-label" :class="{ disabled }">
     <input
-      class="check-input"
+      class="checkbox-input"
       type="checkbox"
-      :value="value"
-      :checked="value"
+      :value="modelValue"
+      :checked="modelValue"
       :disabled="disabled"
-      @change="$emit('change', value)"
+      @change="$emit('update:modelValue', !modelValue)"
     />
-    <svg class="checkbox" viewBox="0 0 20 20">
-      <rect class="check-focus" x="0.5" y="0.5" width="19" height="19" rx="4" />
-      <rect class="check-square" x="3" y="3" width="14" height="14" rx="2" />
-      <polyline class="check-mark" points="5 9 9 13 15 6" />
+    <svg class="checkbox-box" viewBox="0 0 20 20">
+      <defs>
+        <radialGradient id="background-gradient">
+          <stop offset="10%" stop-color="gold" />
+          <stop offset="95%" stop-color="red" />
+        </radialGradient>
+      </defs>
+
+      <rect class="checkbox-focus" x="0.5" y="0.5" width="19" height="19" rx="4" />
+      <rect class="checkbox-square" x="3" y="3" width="14" height="14" rx="2" />
+      <polyline class="checkbox-mark" points="5 9 9 13 15 6" />
     </svg>
     {{ label }}
   </label>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+defineProps<{
+  label: string
+  disabled?: boolean
+  modelValue: boolean
+}>()
 
-export default defineComponent({
-  name: 'BaseCheckbox',
-  props: {
-    label: {
-      type: String,
-      required: true,
-    },
-    value: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    color: {
-      type: String,
-      default: 'default',
-    },
-  },
-  emits: ['change'],
-})
+defineEmits<{
+  (e: 'update:modelValue', value: boolean)
+}>()
 </script>
 
 <style lang="scss" scoped>
-.option {
+.checkbox-label {
   display: block;
   width: fit-content;
-  margin-bottom: 0.5em;
-}
-
-.check {
   padding-left: 1.2em;
+  margin-bottom: 0.5em;
   user-select: none;
+
+  &.disabled {
+    opacity: 0.5;
+  }
 }
 
-.check-disabled {
-  opacity: 0.5;
-}
-
-.check-input {
+.checkbox-input {
   position: absolute;
   appearance: none;
 }
 
-.checkbox {
+.checkbox-box {
   position: absolute;
   width: 1.4em;
   height: 1.4em;
   margin-left: -1.6em;
 }
 
-.check-focus {
+.checkbox-focus {
   fill: none;
 }
 
-.check-square {
+.checkbox-square {
   opacity: 0.25;
-  fill: var(--color-bg);
+  fill: url('#background-gradient');
   stroke: var(--color-text);
   stroke-width: 1px;
 }
 
-.check-mark {
+.checkbox-mark {
   fill: none;
   stroke-width: 2px;
 }
 
-.check-input:disabled + * .check-square {
+.checkbox-input:disabled + * .checkbox-square {
   fill: rgb(234, 234, 234);
   stroke: rgb(155, 155, 155);
 }
 
-.check-input:checked:disabled + * .check-square {
-  fill: rgb(155, 155, 155);
+.checkbox-input:checked:disabled + * .checkbox-square {
+  fill: url('#background-gradient');
 }
 
-.check-input:checked:disabled + * .check-mark {
-  fill: rgb(74, 144, 226);
-}
-
-.check-color-default {
-  .check-input:checked + * .check-mark {
-    stroke: rgb(255, 255, 255);
-  }
-
-  .check-input:checked + * .check-square {
-    fill: rgb(74, 144, 226);
-    stroke: rgb(74, 144, 226);
-  }
-
-  .check-input:focus + * .check-focus {
-    fill: rgb(74, 144, 226);
-  }
-}
-
-@each $color, $color-value in $calculator-colors {
-  .check-color-#{$color} {
-    .check-input:checked + * .check-mark {
-      stroke: map.get($calculator-colors-text, $color);
-    }
-
-    .check-input:checked + * .check-square {
-      fill: $color-value;
-      stroke: $color-value;
-    }
-
-    .check-input:focus + * .check-focus {
-      fill: color.adjust($color-value, $red: -10, $green: -10, $blue: -10);
-    }
-  }
+.checkbox-input:checked + * .checkbox-mark {
+  stroke: rgb(255, 234, 141);
 }
 </style>
