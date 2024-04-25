88e062322dcaaca00bdb10332c586ce37371c966
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2024-04-26 00:39:34 +0500

    fix(locales): prefer location locale
    - use locale from window location (apply 'ru' locale if user enter on '/ru/...' location) if it is not a default application locale (en)

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export type AvailableLocale = 'en' | 'ru'
+
+export const DEFAULT_LOCALE: AvailableLocale = 'en'
+export const AVAILABLE_LOCALES: Readonly<Array<AvailableLocale>> = ['en', 'ru']
+export const INITIAL_LOCATION_LOCALE =
+  AVAILABLE_LOCALES.find((locale) => locale === location.pathname.split('/').at(1)) ?? 'en'
