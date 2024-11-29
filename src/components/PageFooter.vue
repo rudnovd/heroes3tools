255649db819fbab9ede1a9acf3020b3c164068f5
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2024-11-29 21:28:46 +0500

    feat(PageFooter): add telegram link

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -20,6 +20,9 @@
     <a href="https://github.com/rudnovd/heroes3tools" target="_blank" rel="noopener">
       🌟{{ t('components.pageFooter.sourceCode') }}
     </a>
+    <a href="https://t.me/heroes3toolsbot" target="_blank" rel="noopener">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-telegram"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" /></svg>
+    </a>
     <span>{{ t('components.pageFooter.hotaVersion') }}: 1.7.1</span>
     <component
       :is="needRefresh ? 'button' : 'span'"
